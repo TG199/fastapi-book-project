8a893c1f886fa4d0603f6e584a4b8a37aab2786c
Kelechi Ebiri <ebiritg@gmail.com>
2025-02-13 16:17:50 +0000

    fix: change route logic

--- api/routes/books.py
+++ api/routes/books.py
@@ -53,7 +53,7 @@ async def get_book(book_id: int) -> Book:
     if not book:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            content="detail": "Book not found",
+            detail="Book not found",
         )
     return book
 
