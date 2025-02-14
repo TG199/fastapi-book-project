a5e5c777b1a74bbfc9a0929361f4dfc00613f1b8
Kelechi Ebiri <ebiritg@gmail.com>
2025-02-14 13:57:22 +0000

    Trying again with a different server

--- api/routes/books.py
+++ api/routes/books.py
@@ -54,7 +54,7 @@ async def get_book(book_id: int) -> Book:
     if not book:
         return JSONResponse(
                 status_code=status.HTTP_404_NOT_FOUND,
-                content={"detail":"Book not found"},
+                content={"detail":"Book not found!"},
                 )
     return JSONResponse(status_code=status.HTTP_200_OK, content=book.model_dump())
 
